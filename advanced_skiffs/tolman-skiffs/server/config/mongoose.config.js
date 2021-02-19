0bde147be8687cc40d9f7fb5a1d5e0b7c4b12238
TRASJEPS <71234985+TRASJEPS@users.noreply.github.com>
2021-02-18 16:41:55 -0800

    file updates v28

---
+++ advanced_skiffs/tolman-skiffs/server/config/mongoose.config.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose'); //connect to mongoose lib
+const db_name = "skiffs"; //NAME OF DATABASE
+
+mongoose.connect("mongodb://localhost/" + db_name, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    })  
+    .then(() => console.log(`You are conneted to the ${db_name} database!`))
+    .catch((err) => console.log(`error in connecting: ${db_name} database ${err}`));
+    
+
