3a5d0cf0c892709a265eebd53f222a2a2189f32d
TRASJEPS <71234985+TRASJEPS@users.noreply.github.com>
2021-05-07 21:56:38 -0700

    v83 user adds controller WIP levels WIP

--- advanced_skiffs/tolman-skiffs/server/controllers/users.controller.js
+++ advanced_skiffs/tolman-skiffs/server/controllers/users.controller.js
@@ -3,17 +3,20 @@ const User = require('../models/users.model');
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
+// MONGO DB compass 
+// JSON and CSV
+
 module.exports = {
 
     // THIS ALLOWS for SAVING all righin the register method
     register: (req, res) => {
         const user = new User(req.body);
         user
-            // if (user === null ) {
-            //     // EMAIL IS NOT IN SYSTEM
-            //     res.status(400).json({ msg: "Please enter your registered email address."})
-            // }
-            // THEN ADD ELSE?
+            if (user === null ) {
+                // EMAIL IS NOT IN SYSTEM
+                res.status(400).json({ msg: "Please enter your registered email address."})
+            }
+            
             .save()
             .then(() => {
                 res.json({ msg: "Your new profile has been created.", user:user });
@@ -24,9 +27,10 @@ module.exports = {
     login: (req, res) => {
         User.findOne( {email: req.body.email} )
             .then(user => {
+                // SET FLAG FOR FORGET PASSWORD and FIRST TIME LOGIN
                 if (user === null ) {
                     // EMAIL IS NOT IN SYSTEM
-                    res.status(400).json({ msg: "Please enter your registered email address."})
+                    res.status(400).json({ msg: "Please enter your registered email address or contact your HOA management company for access."})
                 }
                 else {
                     bcrypt
@@ -37,7 +41,8 @@ module.exports = {
                                     .cookie("usertoken", jwt.sign({ _id: user._id }, process.env.JWT_SECRET),
                                     {
                                         httpOnly: true,
-                                        // expires: new Date(Date.now() + 99999),   // IF YOU WANT IT TO EXPIRE!
+                                        // USER that has expired // CHANGE TO INACTIVITY // if (activity) then restart the clock 15-10MIN
+                                        // expires: new Date(Date.now() + 99999),   // IF YOU WANT IT TO EXPIRE!  IN SECONDS
                                     })}
                         }
                 }
@@ -46,8 +51,6 @@ module.exports = {
 
 
     },
-
-
 
 
     // NUKE ALL BELOW OR KEEP FOR LATER?
@@ -62,6 +65,9 @@ module.exports = {
                 res.json(err);  //THERE MUST BE A RESPONSE HERE OR IT WAITS INDEFINITELY
             })
     },
+
+// CREATE AN INVITATION FOR A USER TO JOIN - THIS CAN BE THE INTRO OR START TO SEND OFF EMAIL. 
+// THIS WILLBE USED TO MAKE ALL USERS??
 
     create: (req,res) => {
         console.log(req.body);
