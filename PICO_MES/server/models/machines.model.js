361a005b8e4c861e4f214610e0bcc3a9de971749
TRASJEPS <71234985+TRASJEPS@users.noreply.github.com>
2021-06-12 23:26:26 -0700

    v98 PICO_MES update

--- PICO_MES/server/models/machines.model.js
+++ PICO_MES/server/models/machines.model.js
@@ -1,8 +1,17 @@
 const mongoose = require('mongoose');
-
 var Float = require('mongoose-float').loadType(mongoose);
 
 const MachinesSchema = new mongoose.Schema({
+    machineName: {    
+        type: String,
+        required: [true, "Please enter the machine owner's name."], 
+        minLength: [3, "Your owner name must be at least 3 characters long."],
+    },
+    machineModel: {    
+        type: String,
+        required: [true, "Please enter the machine's model."], 
+        minLength: [1, "Your model name must be at least 3 characters long."],
+    },
     ownerName: {    
         type: String,
         required: [true, "Please enter the machine owner's name."], 
@@ -15,22 +24,22 @@ const MachinesSchema = new mongoose.Schema({
         minLength: [3, "Your builder name must be at least 3 characters long."],
     },
 
-    modelName: {   
+    machineType: {   
         type: String,
-        required: [true, "Please select a model name."], 
-        enum: ['Standard', 'Wide Body', 'Jumbo', 'Flat Bottom'],  
+        required: [true, "Please select the type of machine this is."], 
+        enum: ['CNC', '4 Axis', '5 Axis', 'Table Laser'],  
     },
     
     manufactureDate: {   
         type: Date,
-        required: [true, "Please set a a start date."], 
-        min: ['1990-01-01', "Sorry you cannot build a boat before the plans were created.  Please try again."],
-        max: [ new Date(), "You cannot say you are starting a date in the future."], 
+        required: [true, "Please select the date this machine was produced."], 
+        min: ['1990-01-01', "This piece of equipment cannot be more than 100 years old."],
+        max: [ new Date(), "You cannot set this machines manufacture date in the future."], 
     },
     
     dateInstalled: {   
         type: Date,
-        required: [true, "Please set a finish date."], 
+        required: [true, "Please set the date this machine was installed and operational."], 
         min: ['1990-01-01', "Sorry you cannot finish a boat before the plans were created.  Please try again."],
         
     },
@@ -43,7 +52,13 @@ const MachinesSchema = new mongoose.Schema({
     machineCost: {
         type: Float,
         required: [true, "Please enter the cost in dollars and cents."],
-        min: [1, "This machine will cost at least $1.00."],
+        min: [250, "This machine will cost at least $250.00."],
+    },
+
+    yearlyServiceCost: {
+        type: Float,
+        required: [true, "Please enter the cost in dollars and cents."],
+        min: [1000, "Yearly service will cost at least $1000."],
     },
 
     totalMachines: {
